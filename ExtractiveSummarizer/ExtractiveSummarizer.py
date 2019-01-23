c13eaf97c17a68256c7ef2c830d3974194d72804
Emilio Marinone <marinone94@gmail.com>
2019-01-24 00:32:01 +0100

    global comments and instructions

--- ExtractiveSummarizer/ExtractiveSummarizer.py
+++ ExtractiveSummarizer/ExtractiveSummarizer.py
@@ -1,3 +1,17 @@
+"""
+Instructions:
+- open anaconda prompt as administrator
+- naviagate inside the unzipped folder
+- pip install -r requirements.txt
+- python -m spacy download en_core_web_lg
+- python ExtractiveSummarizer.py
+
+Parameters:
+All the parameters are set in config.py
+Test parameters from line 35
+"""
+
+
 from PaperTest.tester import PaperTest
 import nltk
 nltk.download('stopwords')
